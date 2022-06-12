fcdaf06d443bc836a0c49b6671d5388911d13e0e
Mussá Shaukenov <mussa@MacBook-Pro-Mussa.local>
2022-06-13 02:35:52 +0600

    changed the models, settings and admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ProductsModel, CompanyModel
 
-# Register your models here.
+
+admin.site.register(ProductsModel)
+admin.site.register(CompanyModel)
--- app/models.py
+++ app/models.py
@@ -1,7 +1,27 @@
+from tkinter import CASCADE
+from django import db
 from django.db import models
 
-# Create your models here.
+
+class CompanyModel(models.Model):
+    company_name = models.CharField(max_length=32)
+    company_image = models.ImageField(upload_to="images")
+
+    class Meta:
+        db_table = "companies"
+        verbose_name = "company"
+        verbose_name_plural = "companies"
+
+
 class ProductsModel(models.Model):
     product_name = models.CharField(max_length=64)
-    product_price = models.FloatField
+    product_price = models.FloatField()
+    product_image = models.ImageField(upload_to="images")
+    company_name = models.ForeignKey(CompanyModel, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "products"
+        verbose_name = "product"
+        verbose_name_plural = "products"
+
     
