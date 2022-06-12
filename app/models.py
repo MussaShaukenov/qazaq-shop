36036f5e0073cfb472424626101e844764e64186
Mussá Shaukenov <mussa@MacBook-Pro-Mussa.local>
2022-06-12 23:59:12 +0600

    added initial design to header and navbar

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ProductsModel(models.Model):
+    product_name = models.CharField(max_length=64)
+    product_price = models.FloatField
+    
